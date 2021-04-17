502f8a1b7ec4440f23dbb073d9d57e64f41ba497
Gkristof96 <kristef.g@gmail.com>
2021-04-17 09:39:16 +0200

    refactoring js code

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import { Link} from 'react-scroll'
+import React from "react";
+import { Link } from "react-scroll";
 
-const Topbar = ({isOpen, toggle}) => {
-    return (
-        <div className={`topbar${isOpen ? ' open' : ''}`}>
-            <ul className='topbar__menu' >
-                    <li className='topbar__item'><Link onClick={toggle} smooth={true} duration={500} spy={true} exact="true" offset={-81} className='topbar__link' to='trainers'>Trainers</Link></li>
-                    <li className='topbar__item'><Link onClick={toggle} smooth={true} duration={500} spy={true} exact="true" offset={-81} className='topbar__link' to='prices'>Prices</Link></li>
-                    <li className='topbar__item'><Link onClick={toggle} smooth={true} duration={500} spy={true} exact="true" offset={-81} className='topbar__link' to='contact'>Contact</Link></li>
-            </ul>
-        </div>
-    )
-}
+const Topbar = ({ isMenuOpen, toggleMenu }) => {
+  return (
+    <div className={`topbar${isMenuOpen ? " open" : ""}`}>
+      <ul className="topbar__menu">
+        <li className="topbar__item">
+          <Link
+            onClick={toggleMenu}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-81}
+            className="topbar__link"
+            to="trainers"
+          >
+            Trainers
+          </Link>
+        </li>
+        <li className="topbar__item">
+          <Link
+            onClick={toggleMenu}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-81}
+            className="topbar__link"
+            to="prices"
+          >
+            Prices
+          </Link>
+        </li>
+        <li className="topbar__item">
+          <Link
+            onClick={toggleMenu}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-81}
+            className="topbar__link"
+            to="contact"
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Topbar
+export default Topbar;
